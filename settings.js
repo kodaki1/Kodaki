9e838f6a090a7f816cae40cdb4c4544d5fbeaea2
kodaki1 <omarykidumu1@gmail.com>
2025-03-13 23:42:37 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "no one",
+  botOwner: 'hussein', // Your name
+  ownerNumber: '+255619489744', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
